922d0146d3fc7501231bf458ff5f1b71279692d0
Nikolay <Nikolaypronin96@yanex.ru>
2023-08-29 23:14:27 +0400

    added: model - PasswordStorage, serializer -  PasswordSerializer, view - password_list and password_details. Added frontendon React

--- passwordstorage/models.py
+++ passwordstorage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+import datetime
+
+class PasswordStorage(models.Model):
+    title_storage = models.CharField(max_length=150, null=False, blank=False)
+    user_password = models.CharField(max_length=150, null=False, blank=False)
+    discription = models.TextField(max_length=150, null=True, blank=True)
+    create_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title_storage
